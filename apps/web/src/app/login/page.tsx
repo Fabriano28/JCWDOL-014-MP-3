6e2a827d0d8fa3614f4f7fc3396bbaeaf5a6ebed
Fabriano28 <ferumbromin@gmail.com>
2024-08-07 02:26:07 +0700

    feat: added-chakra=themes

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,10 +1,13 @@
+'use client';
+
 import LoginForm from "@/components/Login";
 import { ChakraProvider } from "@chakra-ui/react";
+import { themes } from "@/theme/themes"
 
 export default function LoginPage(){
 
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={themes}>
             <LoginForm />
         </ChakraProvider>
     )
--- apps/web/src/components/Register.tsx
+++ apps/web/src/components/Register.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form"
 import { useRouter } from "next/navigation";
@@ -10,9 +8,12 @@ import {
     FormErrorMessage,
     FormHelperText,
     Input,
+    ButtonGroup,
     Button,
     Box,
     Center,
+    Heading,
+    HStack,
 } from '@chakra-ui/react'
 
 type Inputs = {
@@ -56,65 +57,87 @@ const RegisterForm = () => {
         handleRegister(data);
     }
 
+    const onClickLogin = () => {
+        router.push('/login');
+    }
+
     return (
 
         <div>
             <Center>
                 <Box>
+                    <Center>
+                        <Box mt='5' mb='5'>
+                            <Heading>Register</Heading>
+                        </Box>
+                    </Center>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <FormControl isInvalid={Boolean(errors.email)}>
-                            <FormLabel htmlFor='email'>Email</FormLabel>
-                            <Input
-                                id='email'
-                                placeholder='email'
-                                {...register('email', {
-                                    required: 'This is required',
-                                    minLength: { value: 4, message: 'Minimum length should be 4' },
-                                })}
-                            />
+                        <FormControl variant='floating' isInvalid={Boolean(errors.email)} mb={4}>
+                                <FormLabel htmlFor='email'>Email</FormLabel>
+                                <Input
+                                    id='email'
+                                    placeholder=' '
+                                    {...register('email', {
+                                        required: 'This is required',
+                                        minLength: { value: 4, message: 'Minimum length should be 4' },
+                                    })}
+                                />
                             <FormErrorMessage>
                                 {errors.email && errors.email.message}
                             </FormErrorMessage>
+                        </FormControl>
+                        <FormControl variant='floating' isInvalid={Boolean(errors.email)} mb={4}>
                             <FormLabel htmlFor='first_name'>First Name</FormLabel>
-                            <Input
-                                id='first_name'
-                                placeholder='first_name'
-                                {...register('first_name', {
-                                    required: 'This is required',
-                                    minLength: { value: 4, message: 'Minimum length should be 4' },
-                                })}
-                            />
+                                <Input
+                                    id='first_name'
+                                    placeholder=' '
+                                    {...register('first_name', {
+                                        required: 'This is required',
+                                        minLength: { value: 4, message: 'Minimum length should be 4' },
+                                    })}
+                                />
                             <FormErrorMessage>
                                 {errors.email && errors.email.message}
                             </FormErrorMessage>
-                            <FormLabel htmlFor='last_name'>Last Name</FormLabel>
-                            <Input
-                                id='last_name'
-                                placeholder='last_name'
-                                {...register('last_name', {
-                                    required: 'This is required',
-                                    minLength: { value: 4, message: 'Minimum length should be 4' },
-                                })}
-                            />
+                            </FormControl>
+                            <FormControl variant='floating' isInvalid={Boolean(errors.email)} mb={4}>
+                            <FormLabel htmlFor=' '>Last Name</FormLabel>
+                                <Input
+                                    id='last_name'
+                                    placeholder='last name'
+                                    {...register('last_name', {
+                                        required: 'This is required',
+                                        minLength: { value: 4, message: 'Minimum length should be 4' },
+                                    })}
+                                />
                             <FormErrorMessage>
                                 {errors.email && errors.email.message}
                             </FormErrorMessage>
+                            </FormControl>
+                            <FormControl variant='floating' isInvalid={Boolean(errors.email)} mb={4}>
                             <FormLabel htmlFor='password'>Password</FormLabel>
-                            <Input
-                                id='password'
-                                placeholder='password'
-                                {...register('password', {
-                                    required: 'This is required',
-                                    minLength: { value: 4, message: 'Minimum length should be 4' },
-                                })}
-                            />
+                                <Input
+                                    id='password'
+                                    placeholder=' '
+                                    {...register('password', {
+                                        required: 'This is required',
+                                        minLength: { value: 4, message: 'Minimum length should be 4' },
+                                    })}
+                                />
                             <FormErrorMessage>
                                 {errors.password && errors.password.message}
                             </FormErrorMessage>
                         </FormControl>
-                        <Button mt={4} colorScheme='teal' isLoading={isLoading} type='submit'>
-                            Submit
-                        </Button>
+                        <Center>
+                            <ButtonGroup>
+                                <Button colorScheme='teal' isLoading={isLoading} type='submit'>
+                                    Submit
+                                </Button>
+                                <Button colorScheme='gray' onClick={ onClickLogin }>
+                                    Login
+                                </Button>
+                            </ButtonGroup>
+                        </Center>
                     </form>
                 </Box>
             </Center>
